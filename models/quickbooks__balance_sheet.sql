42fa8cbf1720fcb4004cb4c0d7f9cf9c5e313ff6
robcurtis <12283105+robcurtis@users.noreply.github.com>
2025-01-11 21:58:04 -0800

    Revert back quickbooks__balance_sheet

--- models/quickbooks__balance_sheet.sql
+++ models/quickbooks__balance_sheet.sql
@@ -5,9 +5,9 @@ with general_ledger_by_period as (
     where financial_statement_helper = 'balance_sheet'
 ),  
 
-account_numbers_consolidated as (
+final as (
     select
-        period_first_day as calendar_date,
+        period_first_day as calendar_date, --  Slated to be deprecated; we recommend using `period_first_day` or `period_last_day`
         period_first_day,
         period_last_day,
         source_relation,
@@ -18,27 +18,14 @@ account_numbers_consolidated as (
         parent_account_name,
         account_type,
         account_sub_type,
-        case 
-            when source_relation = 'quickbooks_bdhsc' 
-            and account_number in ('1198', '1199') then '1100'
-            else account_number 
-        end as account_number,
-        max(case 
-            when source_relation = 'quickbooks_bdhsc' 
-            and account_number = '1100' then account_id
-            when source_relation != 'quickbooks_bdhsc' then account_id
-        end) as account_id,
-        max(case 
-            when source_relation = 'quickbooks_bdhsc' 
-            and account_number = '1100' then account_name
-            when source_relation != 'quickbooks_bdhsc' then account_name
-        end) as account_name,
-        sum(period_ending_balance) as amount,
-        sum(period_ending_converted_balance) as converted_amount,
-        min(account_ordinal) as account_ordinal
+        account_number,
+        account_id,
+        account_name,
+        period_ending_balance as amount,
+        period_ending_converted_balance as converted_amount,
+        account_ordinal
     from general_ledger_by_period
-    group by 1,2,3,4,5,6,7,8,9,10,11,12
 )
 
 select *
-from account_numbers_consolidated
+from final
