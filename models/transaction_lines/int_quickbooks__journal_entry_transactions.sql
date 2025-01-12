03db6ae83f968696fd6bc00b4b2e5658227f7d8f
robcurtis <12283105+robcurtis@users.noreply.github.com>
2025-01-12 01:08:34 -0800

    repoint journal_entries to main ar_account

--- models/transaction_lines/int_quickbooks__journal_entry_transactions.sql
+++ models/transaction_lines/int_quickbooks__journal_entry_transactions.sql
@@ -13,6 +13,25 @@ journal_entry_lines as (
     from {{ ref('stg_quickbooks__journal_entry_line') }}
 ),
 
+accounts as (
+    select *
+    from {{ ref('stg_quickbooks__account') }}
+),
+
+default_ar_account as (
+    select distinct
+        first_value(account_id) over (
+            partition by source_relation, currency_id
+            order by account_number
+        ) as default_account_id,
+        currency_id,
+        source_relation
+    from accounts
+    where account_sub_type = 'AccountsReceivable'
+        and is_active
+        and not is_sub_account
+),
+
 final as (
 
     select
@@ -22,7 +41,15 @@ final as (
         journal_entries.doc_number,
         'journal_entry' as transaction_type,
         journal_entries.transaction_date,
-        journal_entry_lines.account_id,
+        coalesce(
+            case 
+                when acct.account_sub_type = 'AccountsReceivable' 
+                and (acct.account_id is null or not acct.is_active)
+                then default_ar.default_account_id
+                else journal_entry_lines.account_id
+            end,
+            journal_entry_lines.account_id
+        ) as account_id,
         journal_entry_lines.class_id,
         journal_entry_lines.department_id,
         journal_entry_lines.customer_id,
@@ -41,9 +68,17 @@ final as (
         journal_entries.total_amount * coalesce(journal_entries.exchange_rate, 1) as total_converted_amount
     from journal_entries
 
-    inner join journal_entry_lines
+    left join journal_entry_lines
         on journal_entries.journal_entry_id = journal_entry_lines.journal_entry_id
         and journal_entries.source_relation = journal_entry_lines.source_relation
+
+    left join accounts as acct
+        on journal_entry_lines.account_id = acct.account_id
+        and journal_entry_lines.source_relation = acct.source_relation
+
+    left join default_ar_account as default_ar
+        on journal_entry_lines.source_relation = default_ar.source_relation
+        and coalesce(acct.currency_id, default_ar.currency_id) = default_ar.currency_id
 )
 
 select *
