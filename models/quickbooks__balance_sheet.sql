16b6a073708277970efc7e818c14d3b2f78165fa
robcurtis <12283105+robcurtis@users.noreply.github.com>
2025-01-13 17:36:34 -0800

    Revert "Update quickbooks__balance_sheet.sql"
    This reverts commit ebe9ae68c7970aa2d22a932bcdf0ab77a3d1f680.

--- models/quickbooks__balance_sheet.sql
+++ models/quickbooks__balance_sheet.sql
@@ -3,27 +3,7 @@ with general_ledger_by_period as (
     select *
     from {{ ref('quickbooks__general_ledger_by_period') }}
     where financial_statement_helper = 'balance_sheet'
-), 
-
-inactive_ar_accounts as (
-    select distinct 
-        account_id,
-        source_relation
-    from (
-        select 
-            account_id,
-            source_relation,
-            period_first_day,
-            period_ending_balance,
-            account_sub_type,
-            sum(case when period_ending_balance = 0 then 1 else 0 end) 
-                over (partition by account_id, source_relation order by period_first_day rows between 2 preceding and current row) as consecutive_zero_months
-        from general_ledger_by_period
-        where period_first_day >= dateadd(month, -12, current_date)
-        and account_sub_type = 'AccountsReceivable'
-    ) zero_balance_check
-    where consecutive_zero_months = 3
-),
+),  
 
 final as (
     select
@@ -45,12 +25,6 @@ final as (
         period_ending_converted_balance as converted_amount,
         account_ordinal
     from general_ledger_by_period
-    where not exists (
-        select 1 
-        from inactive_ar_accounts 
-        where inactive_ar_accounts.account_id = general_ledger_by_period.account_id
-        and inactive_ar_accounts.source_relation = general_ledger_by_period.source_relation
-    )
 )
 
 select *
