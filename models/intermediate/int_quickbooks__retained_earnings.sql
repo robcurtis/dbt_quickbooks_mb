e6ff5400186aa679f033cf188f3e3968772dc404
robcurtis <12283105+robcurtis@users.noreply.github.com>
2025-01-16 01:34:41 -0800

    fix

--- models/intermediate/int_quickbooks__retained_earnings.sql
+++ models/intermediate/int_quickbooks__retained_earnings.sql
@@ -30,23 +30,23 @@ manual_retained_earnings as (
 
 retained_earnings_starter as (
     select
-        cast('9999' as {{ dbt_utils.type_string() }}) as account_id,
+        cast('9999' as {{ dbt.type_string() }}) as account_id,
         nil.source_relation,
-        cast('9999-00' as {{ dbt_utils.type_string() }}) as account_number,
-        cast('Net Income Adjustment' as {{ dbt_utils.type_string() }}) as account_name,
+        cast('9999-00' as {{ dbt.type_string() }}) as account_number,
+        cast('Net Income Adjustment' as {{ dbt.type_string() }}) as account_name,
         false as is_sub_account,
-        cast(null as {{ dbt_utils.type_string() }}) as parent_account_number,
-        cast(null as {{ dbt_utils.type_string() }}) as parent_account_name,
-        cast('Equity' as {{ dbt_utils.type_string() }}) as account_type,
-        cast('RetainedEarnings' as {{ dbt_utils.type_string() }}) as account_sub_type,
-        cast('Equity' as {{ dbt_utils.type_string() }}) as account_class,
-        cast(null as {{ dbt_utils.type_string() }}) as class_id,
-        cast('balance_sheet' as {{ dbt_utils.type_string() }}) as financial_statement_helper,
+        cast(null as {{ dbt.type_string() }}) as parent_account_number,
+        cast(null as {{ dbt.type_string() }}) as parent_account_name,
+        cast('Equity' as {{ dbt.type_string() }}) as account_type,
+        cast('RetainedEarnings' as {{ dbt.type_string() }}) as account_sub_type,
+        cast('Equity' as {{ dbt.type_string() }}) as account_class,
+        cast(null as {{ dbt.type_string() }}) as class_id,
+        cast('balance_sheet' as {{ dbt.type_string() }}) as financial_statement_helper,
         nil.date_year as date_year,
         nil.period_first_day,
         nil.period_last_day as period_last_day,
-        cast(revenue_net_change - expense_net_change + coalesce(mre.manual_re_change, 0) as {{ dbt_utils.type_numeric() }}) as period_net_change,
-        cast(revenue_net_converted_change - expense_net_converted_change + coalesce(mre.manual_re_converted_change, 0) as {{ dbt_utils.type_numeric() }}) as period_net_converted_change
+        cast(revenue_net_change - expense_net_change + coalesce(mre.manual_re_change, 0) as {{ dbt.type_numeric() }}) as period_net_change,
+        cast(revenue_net_converted_change - expense_net_converted_change + coalesce(mre.manual_re_converted_change, 0) as {{ dbt.type_numeric() }}) as period_net_converted_change
     from net_income_loss nil
     left join manual_retained_earnings mre
         on nil.period_first_day = mre.period_first_day
@@ -71,7 +71,7 @@ final as (
         period_first_day,
         period_last_day,
         period_net_change,
-        case when {{ dbt_utils.date_part('month', period_first_day) }} = 1 then cast(0 as {{ dbt_utils.type_numeric() }})
+        case when {{ dbt_utils.date_part('month', period_first_day) }} = 1 then cast(0 as {{ dbt.type_numeric() }})
              else sum(period_net_change) over (
                 partition by source_relation, date_year
                 order by period_first_day
@@ -84,7 +84,7 @@ final as (
             rows between unbounded preceding and current row
         ) as period_ending_balance,
         period_net_converted_change,
-        case when {{ dbt_utils.date_part('month', period_first_day) }} = 1 then cast(0 as {{ dbt_utils.type_numeric() }})
+        case when {{ dbt_utils.date_part('month', period_first_day) }} = 1 then cast(0 as {{ dbt.type_numeric() }})
              else sum(period_net_converted_change) over (
                 partition by source_relation, date_year
                 order by period_first_day
