01fbecf4994d78674680ef11d07d59308b1bc996
robcurtis <12283105+robcurtis@users.noreply.github.com>
2025-01-16 00:36:09 -0800

    retained earnings fix

--- models/intermediate/int_quickbooks__retained_earnings.sql
+++ models/intermediate/int_quickbooks__retained_earnings.sql
@@ -1,11 +1,9 @@
 with general_ledger_balances as (
-
     select *
     from {{ ref('int_quickbooks__general_ledger_balances') }}
 ),
 
 net_income_loss as (
-
     select
         period_first_day,
         date_year,
@@ -15,38 +13,47 @@ net_income_loss as (
         sum(case when account_class = 'Expense' then period_net_change else 0 end) as expense_net_change,
         sum(case when account_class = 'Expense' then period_net_converted_change else 0 end) as expense_net_converted_change
     from general_ledger_balances
-    {{ dbt_utils.group_by(3) }} 
+    group by period_first_day, period_first_day, source_relation
+),
+
+manual_retained_earnings as (
+    select
+        period_first_day,
+        date_year,
+        source_relation,
+        sum(period_net_change) as manual_re_change,
+        sum(period_net_converted_change) as manual_re_converted_change
+    from general_ledger_balances
+    where account_sub_type = 'RetainedEarnings'
+    group by 1, 2, 3
 ),
 
 retained_earnings_starter as (
-
     select
-        cast('9999' as {{ dbt.type_string() }}) as account_id,
-        source_relation,
-        cast('9999-00' as {{ dbt.type_string() }}) as account_number,
-        cast('Net Income Adjustment' as {{ dbt.type_string() }}) as account_name,
+        cast('9999' as {{ dbt_utils.type_string() }}) as account_id,
+        nil.source_relation,
+        cast('9999-00' as {{ dbt_utils.type_string() }}) as account_number,
+        cast('Net Income Adjustment' as {{ dbt_utils.type_string() }}) as account_name,
         false as is_sub_account,
-        cast(null as {{ dbt.type_string() }}) as parent_account_number,
-        cast(null as {{ dbt.type_string() }}) as parent_account_name,
-        cast('Equity' as {{ dbt.type_string() }}) as account_type,
-        cast('RetainedEarnings' as {{ dbt.type_string() }}) as account_sub_type,
-        cast('Equity' as {{ dbt.type_string() }}) as account_class,
-        cast(null as {{ dbt.type_string() }}) as class_id,
-        cast('balance_sheet' as {{ dbt.type_string() }}) as financial_statement_helper,
-        date_year,
-        period_first_day,
-        (date_trunc('month', period_first_day) + interval '1 month' - interval '1 day') as period_last_day,
-        (revenue_net_change - expense_net_change) as period_net_change,
-        cast(0 as {{ dbt.type_numeric() }}) as period_beginning_balance,
-        cast(0 as {{ dbt.type_numeric() }}) as period_ending_balance,
-        (revenue_net_converted_change - expense_net_converted_change) as period_net_converted_change,
-        cast(0 as {{ dbt.type_numeric() }}) as period_beginning_converted_balance,
-        cast(0 as {{ dbt.type_numeric() }}) as period_ending_converted_balance
-    from net_income_loss
+        cast(null as {{ dbt_utils.type_string() }}) as parent_account_number,
+        cast(null as {{ dbt_utils.type_string() }}) as parent_account_name,
+        cast('Equity' as {{ dbt_utils.type_string() }}) as account_type,
+        cast('RetainedEarnings' as {{ dbt_utils.type_string() }}) as account_sub_type,
+        cast('Equity' as {{ dbt_utils.type_string() }}) as account_class,
+        cast(null as {{ dbt_utils.type_string() }}) as class_id,
+        cast('balance_sheet' as {{ dbt_utils.type_string() }}) as financial_statement_helper,
+        nil.date_year as date_year,
+        nil.period_first_day,
+        nil.period_last_day as period_last_day,
+        (revenue_net_change - expense_net_change + coalesce(mre.manual_re_change, 0)) as period_net_change,
+        (revenue_net_converted_change - expense_net_converted_change + coalesce(mre.manual_re_converted_change, 0)) as period_net_converted_change
+    from net_income_loss nil
+    left join manual_retained_earnings mre
+        on nil.period_first_day = mre.period_first_day
+        and nil.source_relation = mre.source_relation
 ),
 
 final as (
-
     select
         account_id,
         source_relation,
@@ -64,34 +71,28 @@ final as (
         period_first_day,
         period_last_day,
         period_net_change,
+        case when extract(month from period_first_day) = 1 then 0
+             else sum(period_net_change) over (
+                partition by source_relation, date_year
+                order by period_first_day
+                rows between unbounded preceding and 1 preceding
+             )
+        end as period_beginning_balance,
         sum(period_net_change) over (
-            partition by source_relation 
-            order by date_year
-            rows between unbounded preceding and 1 preceding
-        ) as period_beginning_balance,
-        sum(period_net_change) over (
-            partition by source_relation 
-            order by date_year
-            rows between unbounded preceding and 1 preceding
-        ) + 
-        sum(period_net_change) over (
-            partition by source_relation, date_year 
+            partition by source_relation, date_year
             order by period_first_day
             rows between unbounded preceding and current row
         ) as period_ending_balance,
         period_net_converted_change,
+        case when extract(month from period_first_day) = 1 then 0
+             else sum(period_net_converted_change) over (
+                partition by source_relation, date_year
+                order by period_first_day
+                rows between unbounded preceding and 1 preceding
+             )
+        end as period_beginning_converted_balance,
         sum(period_net_converted_change) over (
-            partition by source_relation 
-            order by date_year
-            rows between unbounded preceding and 1 preceding
-        ) as period_beginning_converted_balance,
-        sum(period_net_converted_change) over (
-            partition by source_relation 
-            order by date_year
-            rows between unbounded preceding and 1 preceding
-        ) + 
-        sum(period_net_converted_change) over (
-            partition by source_relation, date_year 
+            partition by source_relation, date_year
             order by period_first_day
             rows between unbounded preceding and current row
         ) as period_ending_converted_balance
